643b83660a0082de75456e7b54c304bdce727ab2
Lemuel De Los Santos <lemueld73@gmail.com>
2020-10-12 21:53:48 -0400

    👷 Rollup build update

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,32 +1,31 @@
-import commonjs from "@rollup/plugin-commonjs";
-import resolve from "@rollup/plugin-node-resolve";
-import typescript from "rollup-plugin-typescript2";
-import { terser } from "rollup-plugin-terser";
-import { main, module } from "./package.json";
+import esbuild from "rollup-plugin-esbuild";
+import dts from "rollup-plugin-dts";
+import { main, module, browser, types } from "./package.json";
 
-export default {
-  input: "src/illuminated.ts",
-  output: [
-    {
-      file: main,
-      format: "umd",
-      name: "Illuminated",
-      sourcemap: true
-    },
-    {
-      file: module,
-      format: "es",
-      sourcemap: true
-    }
-  ],
-  plugins: [
-    resolve(),
-    commonjs(),
-    typescript({ useTsconfigDeclarationDir: true }),
-    terser({
-      format: {
-        comments: false
+const input = "src/illuminated.ts";
+
+export default [
+  {
+    input,
+    output: [
+      {
+        file: main,
+        format: "cjs",
+        sourcemap: true
+      },
+      {
+        file: module,
+        format: "es",
+        sourcemap: true
+      },
+      {
+        file: browser,
+        format: "umd",
+        name: "illuminated",
+        sourcemap: true
       }
-    })
-  ]
-};
+    ],
+    plugins: [esbuild({ sourceMap: true, minify: true })]
+  },
+  { input, output: [{ file: types, format: "es" }], plugins: [dts()] }
+];
