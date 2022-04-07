d2e28c7a5fb9b8845afe64185e5df798935dbb64
Lemuel DLS <lemueld73@gmail.com>
2022-04-06 22:59:11 -0400

    ♻️ Minor refactor

--- src/illuminated.ts
+++ src/illuminated.ts
@@ -3,29 +3,16 @@
  * @packageDocumentation
  */
 
-import DarkMask from "./DarkMask";
-import DiscObject from "./DiscObject";
-import Lamp from "./Lamp";
-import Light from "./Light";
-import Lighting from "./Lighting";
-import LineObject from "./LineObject";
-import OpaqueObject from "./OpaqueObject";
-import PolygonObject from "./PolygonObject";
-import RectangleObject from "./RectangleObject";
-import Vec2 from "./Vec2";
+export { default as DarkMask } from "./DarkMask";
+export { default as DiscObject } from "./DiscObject";
+export { default as Lamp } from "./Lamp";
+export { default as Light } from "./Light";
+export { default as Lighting } from "./Lighting";
+export { default as LineObject } from "./LineObject";
+export { default as OpaqueObject } from "./OpaqueObject";
+export { default as PolygonObject } from "./PolygonObject";
+export { default as RectangleObject } from "./RectangleObject";
+export { default as Vec2 } from "./Vec2";
 
 export * from "./constants";
 export * from "./utils";
-
-export {
-  DarkMask,
-  DiscObject,
-  Lamp,
-  Light,
-  Lighting,
-  LineObject,
-  OpaqueObject,
-  PolygonObject,
-  RectangleObject,
-  Vec2
-};
